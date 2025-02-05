ed7236d60800f293fe4f57bb1a9262a995df2f35
JBank25 <urbankjoshua1@gmail.com>
2025-02-04 19:31:38 -0600

    Simple test in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <olc_net.h>
+#include <net_utils.h>
 #include <net_server.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 enum class CustomMsgTypes : uint32_t
 {
@@ -13,31 +16,31 @@ enum class CustomMsgTypes : uint32_t
 
 
 
-class CustomServer : public olc::net::server_interface<CustomMsgTypes>
+class CustomServer : public net::server_interface<CustomMsgTypes>
 {
 public:
-	CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
+	CustomServer(uint16_t nPort) : net::server_interface<CustomMsgTypes>(nPort)
 	{
 
 	}
 
 protected:
-	virtual bool OnClientConnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+	virtual bool OnClientConnect(std::shared_ptr<net::connection<CustomMsgTypes>> client)
 	{
-		olc::net::message<CustomMsgTypes> msg;
+		net::message<CustomMsgTypes> msg;
 		msg.header.id = CustomMsgTypes::ServerAccept;
 		client->Send(msg);
 		return true;
 	}
 
 	// Called when a client appears to have disconnected
-	virtual void OnClientDisconnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+	virtual void OnClientDisconnect(std::shared_ptr<net::connection<CustomMsgTypes>> client)
 	{
 		std::cout << "Removing client [" << client->GetID() << "]\n";
 	}
 
 	// Called when a message arrives
-	virtual void OnMessage(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client, olc::net::message<CustomMsgTypes>& msg)
+	virtual void OnMessage(std::shared_ptr<net::connection<CustomMsgTypes>> client, net::message<CustomMsgTypes>& msg)
 	{
 		switch (msg.header.id)
 		{
@@ -55,7 +58,7 @@ protected:
 			std::cout << "[" << client->GetID() << "]: Message All\n";
 
 			// Construct a new message and send it to all clients
-			olc::net::message<CustomMsgTypes> msg;
+			net::message<CustomMsgTypes> msg;
 			msg.header.id = CustomMsgTypes::ServerMessage;
 			msg << client->GetID();
 			MessageAllClients(msg, client);
@@ -66,17 +69,46 @@ protected:
 	}
 };
 
+void delayedFunction() {
+    // Your original code goes here
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
+        return;
+    }
+
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(60000);
+
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        std::cerr << "Connection failed" << std::endl;
+        return;
+    }
+
+    std::cout << "Connected to localhost on port 60000" << std::endl;
+
+    // Add your communication logic here
+
+    close(sock);
+}
+
+
 int main()
 {
 	CustomServer server(60000); 
 	server.Start();
 
+    std::thread t([]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        delayedFunction();
+    });
+
 	while (1)
 	{
 		server.Update(-1, true);
 	}
-	
-
 
 	return 0;
 }
